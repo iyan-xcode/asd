24678205f297534218c262ea409f927092870c07
Iyan X-Code <robiyan027@gmail.com>
2022-11-18 06:07:08 +0700

    a

---
+++ components/CAdsense.jsx
@@ -0,0 +1,12 @@
+import Script from "next/script";
+import React from "react";
+
+export default function CAdsense() {
+  return (
+    <Script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1150979373977622"
+      crossorigin="anonymous"
+    ></Script>
+  );
+}
